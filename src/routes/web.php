d82750717e2472d1c5c14d5951c5039f4bee0cff
Stas <stasonrs14@gmail.com>
2019-10-02 22:52:10 +0300

    Firs commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/post', function () {
+    return view('post');
+});
+

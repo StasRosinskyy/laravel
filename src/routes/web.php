0e5b1689f2c5dfd7dca304fdc6a5baf105d75595
Stas <stasonrs14@gmail.com>
2019-10-03 14:12:35 +0300

    Commit Message :)

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,11 +15,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
+
 Route::get('/blog', function () {
     return view('blog');
-});
+})->name('blog');
+
 Route::get('/post', function () {
     return view('post');
-});
+})->name('post');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+
+Route::get('/contacts', function () {
+    return view('contacts');
+})->name('contacts');
+
 
